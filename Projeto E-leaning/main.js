865c4896a6cc83b162c7b7e1c3af8c06b49a347a
LivioMarcel <151583236+LivioMarcel@users.noreply.github.com>
2023-11-20 20:02:37 -0300

    Add files via upload

---
+++ Projeto E-leaning/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const cursoMobile = document.getElementById('Mobile');
+
+    cursoMobile.addEventListener('click', function() {
+        window.location.href = 'index2.html'; // Substitua 'outra_pagina.html' pelo nome do arquivo HTML para o qual deseja redirecionar
+    });
+});
