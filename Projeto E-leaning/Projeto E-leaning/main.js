e22794f8b2cd3cede92e3378ce368adb336809ed
LivioMarcel <151583236+LivioMarcel@users.noreply.github.com>
2023-11-20 20:13:51 -0300

    Add files via upload

---
+++ Projeto E-leaning/Projeto E-leaning/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const cursoMobile = document.getElementById('Mobile');
+
+    cursoMobile.addEventListener('click', function() {
+        window.location.href = 'index2.html'; 
+    });
+});
